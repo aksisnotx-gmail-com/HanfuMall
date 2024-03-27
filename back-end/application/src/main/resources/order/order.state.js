58253f981e46d99b46c41c56fbdc4efc6fdf0963
xxl <3578144921@qq.com>
2024-03-27 17:56:19 +0800

    (feat)-2024/3/25: 增加状态JS文件

--- back-end/application/src/main/resources/order/order.state.js
+++ back-end/application/src/main/resources/order/order.state.js
@@ -1,10 +1,10 @@
 const ORDER_STATE = {
-    PLACE_ORDER:{font:'待付款',back:'待付款'},
-    MAKE_PAYMENT:{font:'已付款',back:'待发货'},
-    SHIP_ORDER:{font:'待收货',back:'待收货'},
-    CONFIRM_RECEIPT:{font:'已完成',back:'已完成'},
-    CLOSE_ORDER:{font:'已关闭',back:'已关闭'},
-    APPLY_FOR_REFUND:{font:'待退款',back:'待退款'},
-    REFUND:{font:'已退款',back:'已退款'},
-    DELETE_ORDER:{font:'已删除',back:'已删除'},
+    PLACE_ORDER:{front:'待付款',back:'待付款'},
+    MAKE_PAYMENT:{front:'已付款',back:'待发货'},
+    SHIP_ORDER:{front:'待收货',back:'待收货'},
+    CONFIRM_RECEIPT:{front:'已完成',back:'已完成'},
+    CLOSE_ORDER:{front:'已关闭',back:'已关闭'},
+    APPLY_FOR_REFUND:{front:'待退款',back:'待退款'},
+    REFUND:{front:'已退款',back:'已退款'},
+    DELETE_ORDER:{front:'已删除',back:'已删除'},
 }
